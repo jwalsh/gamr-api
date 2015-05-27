2535862a6ef4320864c26bb1ec9d558ef9a192db
Jason Walsh <j@wal.sh>
2015-05-27 13:01:13 -0400

    Validating compound values for games
    If the user has provided a username assume that all of the associated
    fields need to be provided as well.
    
    This requires that each game grouping be wrapped in a <fieldset>.
    
    This is not wired into .validate() for reporting the messages so the
    console should be used when testing.
    
    With the sample values populated for WoW and a user supplied value for
    LoL (with no region) you should see the following:
    
      game exists:true
      index.js:30 checking compound values for ta_WOW_id
      index.js:39 compound values provided: true
      index.js:30 checking compound values for ta_LOL_id
      index.js:39 compound values provided: false
      index.js:50 provided games valid: false

--- static/index.js
+++ static/index.js
@@ -1,3 +1,51 @@
 console.log('index.js');
 
-$("#id_form").validate();
+$('#id_form').validate();
+
+// Provided games
+var $provided_games = function() {
+    return $('input:text.username')
+        .filter(function() {
+            return $(this).val().trim() !== '';
+        });
+};
+
+// At least one game must have been provided
+var validate_exists_game = function() {
+    return $provided_games().length > 0;
+};
+
+// Any provided game requires all fields be filled
+var validate_provided_games = function() {
+    // The markup for the form needs a container fieldset for all
+    // compound values
+    // $provided_games
+    var valid = true;
+    $('input:text.username')
+        .filter(function() {
+            return $(this).val().trim() !== '';
+        })
+        .map(function() {
+            // If any of the compound values are missing we fail
+            console.log('checking compound values for ' + $(this).attr('id'));
+            var compound_values_provided = $(this)
+                    .parent()
+                    .children()
+                    .filter(
+                        function() {
+                            return $(this).val().trim() === '';
+                        })
+                    .length === 0;
+            console.log('compound values provided: ' +
+                        compound_values_provided);
+            valid = valid && compound_values_provided;
+        });
+    return valid;
+};
+
+$('#validate_game_exists').click(function() {
+    console.log('game exists:' + JSON.stringify(validate_exists_game()));
+    if (validate_exists_game()) {
+        console.log('provided games valid: ' + validate_provided_games());
+    }
+});
